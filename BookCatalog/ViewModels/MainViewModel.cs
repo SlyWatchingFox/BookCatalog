e7657a6b09eef7de563ca46ba0e3c2734aa451e2
Артём <sly.watching.fox@gmail.com>
2023-11-09 10:21:18 +0300

    fix createDB

--- BookCatalog/ViewModels/MainViewModel.cs
+++ BookCatalog/ViewModels/MainViewModel.cs
@@ -69,8 +69,8 @@ public class MainViewModel : ViewModelBase
     {
         try
         {
-            //_booksDb = new BooksDatabase(_host, _port, _userId, _pass);
-            _booksDb = new BooksDatabase("127.0.0.1", 5432, "postgres", "5302");
+            _booksDb = new BooksDatabase(_host, _port, _userId, _pass);
+            //_booksDb = new BooksDatabase("127.0.0.1", 5432, "postgres", "5302");
             ShowConnect = false;
             ShowTable = true;
             _booksDb.ReadTable(Books);
